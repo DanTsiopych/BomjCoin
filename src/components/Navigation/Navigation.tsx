118a9918ebc657c38014dd40daa348f49ac9ce77
Dan Tsiopych <dania2008dd@icloud.com>
2024-08-11 21:39:41 +0300

    Little changes in url of links

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -8,15 +8,15 @@ import Bootle from '../../assets/icons/alcoholic-drink-bottle.svg'
 function Navigation() {
     return (
         <nav className='navigation'>
-            <Link to='secret' className='nav-item'>
+            <Link to='/secret' className='nav-item'>
                 <img src={Mask} className='nav-img' alt='mask-img' />
                 <p className='nav-text'>Secret</p>
             </Link>
-            <Link to='achieves' className='nav-item'>
+            <Link to='/achieves' className='nav-item'>
                 <img src={TinyCoin} className='nav-img' alt='coin-img' />
                 <p className='nav-text'>Achieves</p>
             </Link>
-            <Link to='boosts' className='nav-item'>
+            <Link to='/boosts' className='nav-item'>
                 <img src={Bootle} className='nav-img' alt='bottle-img' />
                 <p className='nav-text'>Kiosk</p>
             </Link>
